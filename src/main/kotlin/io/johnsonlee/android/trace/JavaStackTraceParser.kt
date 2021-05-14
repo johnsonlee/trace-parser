ef3e8e8006dbaa0ddc5af5ffd0c034475152c498
johnsonlee <g.johnsonlee@gmail.com>
2021-05-14 23:19:16 +0800

    Fix bug of JavaStackTraceParser

--- src/main/kotlin/io/johnsonlee/android/trace/JavaStackTraceParser.kt
+++ src/main/kotlin/io/johnsonlee/android/trace/JavaStackTraceParser.kt
@@ -12,7 +12,7 @@ class JavaStackTraceParser {
 
     fun parse(stackTrace: Iterable<String>): List<JavaStackFrame> {
         val frames = mutableListOf<String>()
-        val iterator = stackTrace.iterator()
+        val iterator = stackTrace.findCause().iterator()
 
         while (iterator.hasNext()) {
             iterator.next().takeIf(::isStackTraceElement)?.let(frames::add) ?: if (frames.isNotEmpty()) break
@@ -22,6 +22,28 @@ class JavaStackTraceParser {
     }
 
 
+}
+
+private fun Iterable<String>.findCause(): Iterable<String> {
+    val iterator = iterator()
+    while (iterator.hasNext()) {
+        if (isCause(iterator.next())) {
+            return object : Iterable<String> {
+                override fun iterator(): Iterator<String> = iterator
+            }
+        }
+    }
+    return this
+}
+
+private fun isCause(line: String): Boolean {
+    val causedBy = line.indexOf("Caused by: ").takeIf { it >= 0 } ?: return false
+    for (i in 0 until causedBy) {
+        if (!Character.isWhitespace(line[i])) {
+            return false
+        }
+    }
+    return true
 }
 
 private fun isStackTraceElement(line: String): Boolean {
