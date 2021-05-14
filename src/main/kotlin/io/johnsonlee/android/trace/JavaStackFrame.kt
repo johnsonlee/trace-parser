aca22c2b6b8072b0586e7e1b467e80aaa55f14bc
johnsonlee <g.johnsonlee@gmail.com>
2021-05-15 01:28:20 +0800

    Fix bug of java stack trace parsing

--- src/main/kotlin/io/johnsonlee/android/trace/JavaStackFrame.kt
+++ src/main/kotlin/io/johnsonlee/android/trace/JavaStackFrame.kt
@@ -9,9 +9,42 @@ private const val INVALID_CLASS_NAME: String = ""
 
 private const val INVALID_METHOD_NAME: String = ""
 
-private val systemPackages = setOf("java.", "kotlin.", "android.", "androidx.", "com.android.", "dalvik.")
+private val systemPackages = setOf(
+        "java.",
+        "kotlin.",
+        "android.",
+        "androidx.",
+        "com.android.",
+        "dalvik."
+)
 
 class JavaStackFrame(snapshot: String) : StackFrame(snapshot) {
+
+    private val at: Int? by lazy {
+        snapshot.indexOf("at ").takeIf { it > -1 }
+    }
+
+    private val rbrace: Int? by lazy {
+        snapshot.lastIndexOf(')').takeIf { it > -1 }
+    }
+
+    private val colon: Int? by lazy {
+        rbrace?.let { r ->
+            snapshot.lastIndexOf(':', r).takeIf { it > -1 }
+        }
+    }
+
+    private val lbrace: Int? by lazy {
+        (colon ?: rbrace)?.let { r ->
+            snapshot.lastIndexOf('(', r).takeIf { it > -1 }
+        }
+    }
+
+    private val dot: Int? by lazy {
+        lbrace?.let { l ->
+            snapshot.lastIndexOf('.', l).takeIf { it > -1 }
+        }
+    }
 
     override val isFromUser: Boolean by lazy {
         systemPackages.none {
@@ -20,46 +53,29 @@ class JavaStackFrame(snapshot: String) : StackFrame(snapshot) {
     }
 
     val className: String by lazy {
-        val at = snapshot.indexOf("at ").takeIf { it > -1 } ?: return@lazy INVALID_CLASS_NAME
-        val brace = snapshot.lastIndexOf('(').takeIf { it > at } ?: return@lazy INVALID_CLASS_NAME
-        val dot = snapshot.lastIndexOf('.', brace).takeIf { it in (at + 1) until brace } ?: return@lazy INVALID_CLASS_NAME
-        val slash = snapshot.lastIndexOf('/', brace)
-        snapshot.substring(slash.takeIf { it > -1 } ?: (at + 3), dot)
+        val at = this.at ?: return@lazy INVALID_CLASS_NAME
+        val dot = this.dot ?: return@lazy INVALID_CLASS_NAME
+        val slash = snapshot.lastIndexOf('/', dot).takeIf { it > -1 }
+        snapshot.substring(slash ?: (at + 3), dot)
     }
 
     val methodName: String by lazy {
-        val at = snapshot.indexOf("at ").takeIf { it > -1 } ?: return@lazy INVALID_METHOD_NAME
-        val brace = snapshot.lastIndexOf('(').takeIf { it > at } ?: return@lazy INVALID_METHOD_NAME
-        val dot = snapshot.lastIndexOf('.', brace).takeIf { it in (at + 1) until brace } ?: return@lazy INVALID_METHOD_NAME
-        snapshot.substring(dot + 1, brace)
+        val lbrace = this.lbrace ?: return@lazy INVALID_METHOD_NAME
+        val dot = this.dot ?: return@lazy INVALID_METHOD_NAME
+        snapshot.substring(dot + 1, lbrace)
     }
 
     val sourceFile: String by lazy {
-        val lbrace = snapshot.indexOf('(')
-        val rbrace = snapshot.indexOf(')')
-        val colon = snapshot.lastIndexOf(':')
-
-        if (lbrace <= -1 || rbrace <= -1) {
-            INVALID_FILE_NAME
-        } else if (colon <= -1) {
-            snapshot.substring(lbrace + 1, rbrace)
-        } else {
-            snapshot.substring(lbrace + 1, colon)
-        }
+        val lbrace = this.lbrace ?: return@lazy INVALID_FILE_NAME
+        val rbrace = this.rbrace ?: return@lazy INVALID_FILE_NAME
+        snapshot.substring(lbrace + 1, this.colon ?: rbrace)
     }
 
     val lineNumber: Int by lazy {
-        val brace = snapshot.lastIndexOf(')')
-        val colon = snapshot.lastIndexOf(':')
-        if (brace > colon && colon > -1) {
-            try {
-                snapshot.substring(colon + 1, brace).toInt()
-            } catch (e: NumberFormatException) {
-                INVALID_LINE_NUMBER
-            }
-        } else {
-            INVALID_LINE_NUMBER
-        }
+        val rbrace = this.rbrace ?: return@lazy INVALID_LINE_NUMBER
+        this.colon?.let {
+            snapshot.substring(it + 1, rbrace).toInt()
+        } ?: INVALID_LINE_NUMBER
     }
 
 }
